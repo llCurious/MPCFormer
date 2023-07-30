fix bug of softmax in quan_gpt2

src/main/transformer/quant_gpt2_modeling.py
@@ -1154,7 +1154,7 @@ class IntSoftmax(nn.Module):
         scaling_factor = exp_scaling_factor / 2**self.n
         return exp_int, scaling_factor
 
-    def forward(self, x, dim=-1, scaling_factor=None):
+    def forward(self, x, mask, dim=-1, scaling_factor=None):
         if self.quant_mode == "none":
             return utils.softmax(x, dim=-1, onnx_trace=False), None
 